f16fab296e773c38b659fab94c4c9fc73f396ae3
Valentin Curiel <valentincuriel03@gmail.com>
2024-10-21 16:52:06 -0300

    Se agrego AppRouter

--- ProyectoFinalReact/src/App.tsx
+++ ProyectoFinalReact/src/App.tsx
@@ -1,15 +1,17 @@
 import NavBar from "./components/NavBar";
 import CuerpoPrincipal from "./components/CuerpoPrincipal";
 import AsideEmpresa from "./components/AsideEmpresa";
+import { AppRouter } from "./routes/AppRouter";
 
 function App() {
-  return (
-    <div className="parent">
-      <NavBar />
-      <CuerpoPrincipal />
-      <AsideEmpresa />
-    </div>
-  );
+	return (
+		<div className="parent">
+			<AppRouter />
+			<NavBar />
+			<CuerpoPrincipal />
+			<AsideEmpresa />
+		</div>
+	);
 }
 
 export default App;
